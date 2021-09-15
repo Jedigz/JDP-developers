1ca53bd4bcbfe2bf2b4ce740c1645a4a408f261b
Jose Fernando Higuera <90433304+joseH13@users.noreply.github.com>
2021-09-15 15:50:37 -0500

    Update Mesas.cs

--- UCR.App.Dominio/Entidades/Mesas.cs
+++ UCR.App.Dominio/Entidades/Mesas.cs
@@ -1,1 +1,12 @@
 using System;
+namespace UCR.App.Dominio
+{
+  public class Mesa
+  {
+    public int MesaId{get;set;}
+    public int cantidadSillas{get;set;}
+    public bool disponibilidad{get;set;}
+    public int IdTurno{get;set;}
+  }
+
+}
