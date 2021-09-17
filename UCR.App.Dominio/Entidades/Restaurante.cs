dd49a08853dd9ecbebfac42c64708a430fc00849
joseH13 <jfhr06@gmail.com>
2021-09-16 23:24:41 -0500

    modificación mesa

--- UCR.App.Dominio/Entidades/Restaurante.cs
+++ UCR.App.Dominio/Entidades/Restaurante.cs
@@ -1,8 +1,11 @@
 using System;
 namespace UCR.App.Dominio
 { 
+  public class Restaurante
+  {
   public int RestauranteId{get;set;}
   public int aforo{get;set;}
   public System.collections.Generic.List<Turno> turnos{get;set;}
   public System.collections.Generic.List<Mesa> mesas{get;set;}
+  }
 }
