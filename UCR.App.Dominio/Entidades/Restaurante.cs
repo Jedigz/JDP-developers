0683c82846e82f85e394fe304ccef05e3b9eb5d0
Jose Fernando Higuera <90433304+joseH13@users.noreply.github.com>
2021-09-17 00:04:32 -0500

    Clase finalizada

--- UCR.App.Dominio/Entidades/Restaurante.cs
+++ UCR.App.Dominio/Entidades/Restaurante.cs
@@ -3,9 +3,20 @@ namespace UCR.App.Dominio
 { 
   public class Restaurante
   {
-  public int RestauranteId{get;set;}
-  public int aforo{get;set;}
-  public System.collections.Generic.List<Turno> turnos{get;set;}
-  public System.collections.Generic.List<Mesa> mesas{get;set;}
+    public int RestauranteId{get;set;}
+    public int aforo{get;set;}
+    public System.collections.Generic.List<Turno> turnos{get;set;}
+    public System.collections.Generic.List<Mesa> mesas{get;set;}
+    public void RevisarDisponibilidad()
+    
+    {
+      Console.WriteLine("aun es necesario implementar esta clase");
+    }
+    public void LlenarMesa()
+    {
+      Console.WriteLine("aun es necesario implementar esta clase");
+    }
+    
+  
   }
 }
