02ad71c44edf6ea9e0dc433c3183f42ec710e463
Cpmoramu1 <90433530+Cpmoramu1@users.noreply.github.com>
2021-09-17 23:05:37 -0500

    Adjunto clase No Contagiado

--- UCR.App.Dominio/Entidades/NOcontagiado.cs
+++ UCR.App.Dominio/Entidades/NOcontagiado.cs
@@ -1,1 +1,14 @@
+using System;
+
+namespace UCR.App.Dominio
+{
+    public class NoContagiado
+    {
+        public System.Collections.Generic.List<Persona>personas{get;set;}
+        public bool rehabilitado {get;set;}
+
+    
+    }
+
+}
 
