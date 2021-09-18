cc307e42ff0b741bd61577c4734baf4287613ca7
Cpmoramu1 <90433530+Cpmoramu1@users.noreply.github.com>
2021-09-17 23:06:35 -0500

    Adjunto clase Turno

--- UCR.App.Dominio/Entidades/Turno.cs
+++ UCR.App.Dominio/Entidades/Turno.cs
@@ -1,1 +1,15 @@
+using System;
+
+namespace UCR.App.Dominio 
+{
+    public class Turno 
+    {
+        public NoContagiado nocontagiado {get;set;}
+        public DateTime horarioentrada  {get;set;}
+        public string menu {get;set;}
+        public int idmesa {get;set;}
+        public int idturno {get;set;} 
+    }
+
+}
 
