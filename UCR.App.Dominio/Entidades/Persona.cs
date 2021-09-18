7f25a37920cae90e1f4925b0e28deadc65a57c6c
Daniel Torres <90433482+dmtg28@users.noreply.github.com>
2021-09-17 22:55:51 -0500

    Cargada clase Persona

--- UCR.App.Dominio/Entidades/Persona.cs
+++ UCR.App.Dominio/Entidades/Persona.cs
@@ -3,13 +3,13 @@ using System;
 namespace  UCR.App.Dominio
 
 {
-    public class persona 
+    public class Persona 
     {
         public string nombre {get;set;}
         public string apellido {get;set;}
         public int edad {get;set;}
         public bool estadoCovid {get;set;}
-        
+        public Administrativo tipoAdministrativo {get;set;}
     }
 }
 
