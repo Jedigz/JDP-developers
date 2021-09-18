c7eee2bfeea8247d4c7bee69074dc6a420f60e0e
jorge4901 <jorgedigaleano@hotmail.com>
2021-09-17 20:49:09 -0500

    Se actualizan entidades Contagiado y Rehabilitado

--- UCR.App.Dominio/Entidades/Contagiado.cs
+++ UCR.App.Dominio/Entidades/Contagiado.cs
@@ -1,1 +1,16 @@
+using System;
 
+namespace UCR.App.Dominio
+{
+    public class Contagiado
+    {
+        public string sintomas{get;set;}
+
+        public DateTime fecha{get;set;}
+
+        public DateTime limiteAislamiento{get;set;}
+
+        //Falta implementar el metodo de analisis
+
+    }
+}
--- UCR.App.Dominio/Entidades/Rehabilitado.cs
+++ UCR.App.Dominio/Entidades/Rehabilitado.cs
@@ -1,1 +1,11 @@
+using System;
 
+namespace UCR.App.Dominio
+{
+    public class Rehabilitado
+    {
+
+        public System.Collections.Generic.List<Contagiado> contagiados{get;set;}
+
+    }
+}
