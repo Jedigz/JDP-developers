c577d7b86ba371bd9a54bd7cb95aeadb9f568ec9
Daniel Torres <90433482+dmtg28@users.noreply.github.com>
2021-09-17 22:39:40 -0500

    Se sube la clase persona

--- UCR.App.Dominio/Entidades/Persona.cs
+++ UCR.App.Dominio/Entidades/Persona.cs
@@ -1,1 +1,15 @@
+using System;
+
+namespace  UCR.App.Dominio
+
+{
+    public class persona 
+    {
+        public string nombre {get;set;}
+        public string apellido {get;set;}
+        public int edad {get;set;}
+        public bool estadoCovid {get;set;}
+        
+    }
+}
 
