8472bdc8ae2c2b0159c6c7ef2f1715dae15022a0
Daniel Torres <90433482+dmtg28@users.noreply.github.com>
2021-09-22 23:19:05 -0500

    Class administrativo

--- UCR.App.Dominio/Entidades/Administrativo.cs
+++ UCR.App.Dominio/Entidades/Administrativo.cs
@@ -6,7 +6,7 @@ namespace UCR.App.Dominio
     public class Administrativo : Persona
     {
         public string dependencia {get;set;}
-        public int oficina {get;set;}
+        public string oficina {get;set;}
     
     }
 
