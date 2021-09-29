5164b10424e0347f8cf544870d35e3da883909d3
Daniel Torres <90433482+dmtg28@users.noreply.github.com>
2021-09-28 22:11:31 -0500

    Update IRepositorioTurno.cs

--- UCR.App.Persistencia/AppContext/IRepositorioTurno.cs
+++ UCR.App.Persistencia/AppContext/IRepositorioTurno.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
-using UCP.App.Dominio;
+using UCR.App.Dominio;
 
-namespace UCP.App.Persistencia
+namespace UCR.App.Persistencia
 {
     public interface IRepositorioTurno
     {
